65754e97a148b08af8ba5c9fa7185c159a5949a6
Saurabh Verma <saurabh1088@gmail.com>
2023-03-10 23:27:24 +0530

    Added playground for learning combine framework.

---
+++ Combine.playground/Contents.swift
@@ -0,0 +1,32 @@
+// Created by Saurabh Verma on 03/03/23
+// Copyright © 2023 Saurabh Verma, (saurabh1088@gmail.com). All rights reserved.
+
+/**
+ `What is Combine?`
+ 
+ Combine is a framework developed by Apple. It aims to provide declarative APIs for processing values over time.
+ This providing of values over time is something we will come to know as publishers, but later on to that.
+ This framework was introduced in WWDC 2019
+ https://developer.apple.com/videos/play/wwdc2019/721
+ https://developer.apple.com/videos/play/wwdc2019/722
+ 
+ Combine enables us to write funtional reactive code by using it's declarative API. So it provides the
+ functional reative programming (FRP) paradigm capability to Swift.
+ 
+ For e.g. in SwiftUI framework the `ObservableObject` and `@Published` are actually using Combine
+ behind the scenes.
+ 
+ `Functional Reactive Programming`
+ 
+ Before proceeding with Combine in details, it's necessary to understand what is FRP i.e. Functional Reactive Programming.
+ Functional Reactive Programming is a programming paradigm.
+ 
+ `Combine core concepts`
+ - Publishers
+ - Subscribers
+ - Operators
+ - Subjects
+ 
+ 
+ 
+ */
