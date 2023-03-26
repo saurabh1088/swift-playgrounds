ce840090e688e557a7d6b737553a8b023f9d5c66
Saurabh Verma <saurabh1088@gmail.com>
2023-03-26 23:32:41 +0530

    Updated some comments for factory pattern and added a TODO readout.

--- DesignPatterns/CreationalDesignPatterns.playground/Pages/Factory.xcplaygroundpage/Contents.swift
+++ DesignPatterns/CreationalDesignPatterns.playground/Pages/Factory.xcplaygroundpage/Contents.swift
@@ -6,12 +6,18 @@
 /**
  `Factory`
  
- `Factory` is a creational design pattern, do note however it's not at all related to `Abstract Factory`.
- As creational design patterns are all about removing the complexities while creating objects, so is the case with
- factory pattern. `Factory` pattern tries to encapsulate implementation details for creating objecs which have
- a common ancestor i.e. base class.
+ `Factory` is a creational design pattern, as creational design patterns are all about removing the complexities
+ while creating objects, so is the case with factory pattern.
+ `Factory` pattern tries to encapsulate implementation details for creating objecs which have a common ancestor i.e. base class.
  In `Factory` pattern the clien uses a product object which it receives from a common interface without caring
  about the exact type of concrete object returned via `Factory`
+ 
+ In this pattern there is a `Factory` which creates some objects. These objects are the products, client needs
+ eventualy and asks a `Factory` to provide those.
+ 
+ `How to differentiate Factory pattern with Abstract Factory pattern?`
+ Good Explanation :-
+ https://stackoverflow.com/questions/5739611/what-are-the-differences-between-abstract-factory-and-factory-design-patterns
  */
 
 import Foundation
