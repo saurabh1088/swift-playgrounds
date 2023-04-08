02cefa4915037ddd230f3c41be25c2942d0707e1
Saurabh Verma <saurabh1088@gmail.com>
2023-04-08 07:53:06 +0530

    Added example 2 for KVO using publisher.

--- Combine.playground/Pages/Key Value Observing using Combine.xcplaygroundpage/Contents.swift
+++ Combine.playground/Pages/Key Value Observing using Combine.xcplaygroundpage/Contents.swift
@@ -11,6 +11,9 @@ import Combine
 import UIKit
 import PlaygroundSupport
 
+/// Example 1 :
+/// This example uses KVO. The name property of model Employee is observed using KVO and when it gets
+/// updated then a print statement is executed.
 class Employee: NSObject {
     //TODO: Without adding dynamic here this example doesn't works, find out why.
     @objc dynamic var name: String
@@ -24,16 +27,26 @@ class Employee: NSObject {
 class MyViewController: UIViewController {
     @objc var employee: Employee? = nil
     var observation: NSKeyValueObservation?
+    let button = UIButton()
     
     override func loadView() {
-        self.view = UIView()
+        let view = UIView()
         view.backgroundColor = .red
+        button.frame = CGRect(x: 0, y: 0, width: 200, height: 40)
+        button.setTitle("Button", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        view.addSubview(button)
+        button.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 1).isActive = true
+        button.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 1).isActive = true
+        self.view = view
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        button.setTitle(employee?.name, for: .normal)
         observation =  employee?.observe(\.name, options: [.new], changeHandler: { object, change in
             print("Employee changed name : \(change.newValue)")
+            self.button.setTitle(self.employee?.name, for: .normal)
         })
     }
     
@@ -44,8 +57,54 @@ class MyViewController: UIViewController {
     
 }
 
-let myViewController = MyViewController()
-myViewController.employee = Employee(name: "Batman")
+
+//let myViewController = MyViewController()
+//myViewController.employee = Employee(name: "Batman")
+//PlaygroundPage.current.needsIndefiniteExecution = true
+//PlaygroundPage.current.liveView = myViewController
+
+
+
+/// Example 2 :
+/// Now same example as above will try with using combine.
+
+class MyViewControllerUsingCombine: UIViewController {
+    @objc var employee: Employee? = nil
+    var cancellable: Cancellable?
+    let button = UIButton()
+    
+    override func loadView() {
+        let view = UIView()
+        view.backgroundColor = .red
+        button.frame = CGRect(x: 0, y: 0, width: 200, height: 40)
+        button.setTitle("Button", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        view.addSubview(button)
+        button.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 1).isActive = true
+        button.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 1).isActive = true
+        self.view = view
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        button.setTitle(employee?.name, for: .normal)
+        cancellable = employee?.publisher(for: \.name)
+            .sink(receiveValue: { receivedValue in
+                print("Employee changed name : \(receivedValue)")
+                self.button.setTitle(self.employee?.name, for: .normal)
+            })
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        employee?.name = "Man of steel"
+    }
+    
+}
+
+let combineViewController = MyViewControllerUsingCombine()
+combineViewController.employee = Employee(name: "Superman")
 PlaygroundPage.current.needsIndefiniteExecution = true
-PlaygroundPage.current.liveView = myViewController
+PlaygroundPage.current.liveView = combineViewController
+
 //: [Next](@next)
