aff9cfb0738c4f46d80c4eff0bbe281e1be28092
Saurabh Verma <saurabh1088@gmail.com>
2023-05-24 23:54:46 +0530

    Added playground to explore structural design patterns in Swift.

---
+++ DesignPatterns/StructuralDesignPatterns.playground/Pages/Adapter.xcplaygroundpage/Contents.swift
@@ -0,0 +1,13 @@
+//: [Previous](@previous)
+
+// Created by Saurabh Verma on 24/05/23
+// Copyright © 2023 Saurabh Verma, (saurabh1088@gmail.com). All rights reserved.
+
+/**
+ `Adapter`
+ `Adapter` design pattern is a structural design pattern that allows objects with incompatible interfaces to work
+ together. In other words, it transforms the interface of an object to adapt it to a different object.
+ */
+import Foundation
+
+//: [Next](@next)
---
+++ DesignPatterns/StructuralDesignPatterns.playground/Pages/Bridge.xcplaygroundpage/Contents.swift
@@ -0,0 +1,4 @@
+// Created by Saurabh Verma on 24/05/23
+// Copyright © 2023 Saurabh Verma, (saurabh1088@gmail.com). All rights reserved.
+
+import UIKit
