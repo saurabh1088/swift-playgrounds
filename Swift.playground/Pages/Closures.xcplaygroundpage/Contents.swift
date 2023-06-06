7d29c7d0fb86083d73ff9b84a20e095b9adb3c5f
Saurabh Verma <saurabh1088@gmail.com>
2023-06-06 23:45:52 +0530

    Added playground page to explore closures.

---
+++ Swift.playground/Pages/Closures.xcplaygroundpage/Contents.swift
@@ -0,0 +1,27 @@
+//: [Previous](@previous)
+
+// Created by Saurabh Verma on 06/06/23
+// Copyright © 2023 Saurabh Verma, (saurabh1088@gmail.com). All rights reserved.
+
+/**
+ `Closures`
+ 
+ Quoting from Apple's documentation :
+ `Closures are self-contained blocks of functionality that can be passed around and used in your code.`
+ 
+ Closures are similar to what we have as blocks in Objective C and C. Also similar to concepts like lambdas in
+ some other languages.
+ 
+ Closures can be
+ - Assigned to variables
+ - Passed as arguments to functions
+ - Returned from functions
+ - Can capture and store references to any constant or variables from context in which closure is defined
+ 
+ Capturing of constants or variables by closure, from the context in which closure is defined is known as
+ `closing over` those constants and variables.
+ 
+ */
+import Foundation
+
+//: [Next](@next)
