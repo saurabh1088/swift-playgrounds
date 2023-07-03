839f2f70bc59b59208f9f3e41c49375e58f206aa
Saurabh Verma <saurabh1088@gmail.com>
2023-07-03 21:05:30 +0530

    Added new playground to explore SOLID principles.

---
+++ DesignPatterns/SOLID.playground/Contents.swift
@@ -0,0 +1,17 @@
+// Created by Saurabh Verma on 03/07/23
+// Copyright © 2023 Saurabh Verma, (saurabh1088@gmail.com). All rights reserved.
+
+/**
+ `SOLID Design Principles`
+ 
+ Introduced by Robert C Martin (Uncle Bob)
+ 
+ `Single Responsibility Principle`
+ 
+ A class should have only one reason to change.
+ 
+ */
+
+import UIKit
+
+var greeting = "Hello, playground"
