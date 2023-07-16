5f9af3c44e471838976a619a165020d39d8cf20a
Saurabh Verma <saurabh1088@gmail.com>
2023-07-16 23:25:02 +0530

    Added new playground page for documenting facade structural design pattern.

---
+++ DesignPatterns/StructuralDesignPatterns.playground/Pages/Facade.xcplaygroundpage/Contents.swift
@@ -0,0 +1,15 @@
+//: [Previous](@previous)
+
+// Created by Saurabh Verma on 16/07/23
+// Copyright © 2023 Saurabh Verma, (saurabh1088@gmail.com). All rights reserved.
+
+/**
+ `Facade`
+ Facade structural design patterns aims in exposing several components through a single interface, thereby
+ providing a simple, easy to use and understand, user interface over a large and sophisticated body of code.
+ */
+import Foundation
+
+var greeting = "Hello, playground"
+
+//: [Next](@next)
