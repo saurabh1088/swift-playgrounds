868b6058aca75bcd76f23bcaafc8c5444ebb7326
Saurabh Verma <saurabh1088@gmail.com>
2023-07-20 23:33:39 +0530

    Added playground page to explore flyweight structural design pattern.

---
+++ DesignPatterns/StructuralDesignPatterns.playground/Pages/Flyweight.xcplaygroundpage/Contents.swift
@@ -0,0 +1,16 @@
+//: [Previous](@previous)
+
+// Created by Saurabh Verma on 20/07/23
+// Copyright © 2023 Saurabh Verma, (saurabh1088@gmail.com). All rights reserved.
+
+/**
+ `Flyweight`
+ Flyweight design pattern aims to optimize memory usage by sharing common data between multiple objects.
+ This pattern can be used when a large number of similar objects are getting genarated such that one can have
+ some shared intrinsic data among these to have more efficiency.
+ */
+import Foundation
+
+var greeting = "Hello, playground"
+
+//: [Next](@next)
