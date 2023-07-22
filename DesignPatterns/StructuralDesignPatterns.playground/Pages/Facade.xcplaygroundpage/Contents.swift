18097a87f9c0b1a40deb25f551168cb5d93a0218
Saurabh Verma <saurabh1088@gmail.com>
2023-07-22 10:31:39 +0530

    Added example for Facade design pattern.

--- DesignPatterns/StructuralDesignPatterns.playground/Pages/Facade.xcplaygroundpage/Contents.swift
+++ DesignPatterns/StructuralDesignPatterns.playground/Pages/Facade.xcplaygroundpage/Contents.swift
@@ -10,6 +10,53 @@
  */
 import Foundation
 
-var greeting = "Hello, playground"
+class CivilEngineer {
+    func designBuildingPlan() {
+        print("Building plan designed")
+    }
+}
 
+class Worker {
+    func constructBuilding() {
+        print("Constructing building...")
+    }
+}
+
+class Necessities {
+    func addNecessities() {
+        print("Necessities plumbing, wiring etc added")
+    }
+}
+
+class Builder {
+    let engineer: CivilEngineer
+    let workers: [Worker]
+    let necessities: Necessities
+    
+    init(engineer: CivilEngineer, workers: [Worker], necessities: Necessities) {
+        self.engineer = engineer
+        self.workers = workers
+        self.necessities = necessities
+    }
+    
+    func constructProject() {
+        engineer.designBuildingPlan()
+        workers.map({ $0.constructBuilding() })
+        necessities.addNecessities()
+        print("Project completed")
+    }
+}
+
+func exampleFacadePattern() {
+    let engineer = CivilEngineer()
+    let worker1 = Worker()
+    let worker2 = Worker()
+    let necessities = Necessities()
+    let builder = Builder(engineer: engineer,
+                          workers: [worker1, worker2],
+                          necessities: necessities)
+    builder.constructProject()
+}
+
+exampleFacadePattern()
 //: [Flyweight Pattern ->](@next)
