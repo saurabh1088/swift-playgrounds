723a07a916bbf03260db8746126b72db1e20d164
Saurabh Verma <saurabh1088@gmail.com>
2023-07-23 15:13:19 +0530

    Added comments for example of facade design pattern.

--- DesignPatterns/StructuralDesignPatterns.playground/Pages/Facade.xcplaygroundpage/Contents.swift
+++ DesignPatterns/StructuralDesignPatterns.playground/Pages/Facade.xcplaygroundpage/Contents.swift
@@ -10,6 +10,17 @@
  */
 import Foundation
 
+/// Example 1 :
+/// Example below describes how a facade pattern is implemented and used. Here we have some classes
+/// providing APIs for giving various solutions for creating and delivering a real estate project. A real estate project
+/// will need a civil engineer to design the plan, some workers to construct the buildings as per plan and some
+/// basic requirements like electrical wiring, gas connection, plumbing etc which are covered under necessities.
+/// Class `Builder` is a facade here which uses all these APIs and creates the project. So for an end client
+/// the `Builder` interface and it's APIs are much easier to use and manage.
+/// Also it helps in situations where say instead of using `Builder` one uses `CivilEngineer`, `Worker`
+/// and `Necessities` individually and then in future the implementation of `CivilEngineer`, `Worker`
+/// or `Necessities` changes, then one will need to change a lot of code. Instead if all these were being used
+/// via facade `Builder` only change will be required in the `Builder`.
 class CivilEngineer {
     func designBuildingPlan() {
         print("Building plan designed")
