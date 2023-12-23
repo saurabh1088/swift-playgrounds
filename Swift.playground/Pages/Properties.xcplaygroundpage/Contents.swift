fc7c1f43f922b0bf63d9d1489ee7fef1b8fa95ef
Saurabh Verma <saurabh1088@gmail.com>
2023-12-23 13:31:46 +0530

    Swift : Added example for defining a custom property wrapper.

--- Swift.playground/Pages/Properties.xcplaygroundpage/Contents.swift
+++ Swift.playground/Pages/Properties.xcplaygroundpage/Contents.swift
@@ -93,9 +93,40 @@ func exampleForLazyProperty() {
 /// there is no gurantee that it will get initialized only once.
 
 /// `Property Wrappers`
-///
-///
+/// Concept of property wrapper in Swift helps in defining a layer which separates the code defining a property
+/// from code which manages how the property is stored.
+/// Use case for property wrapper is that suppose we have to define a property on which one needs to apply some
+/// custom logic, for example we want to define a property and make sure it is thread safe, then those thread
+/// safety check needs to go for all properties which needs such feature. Using property wrapper one can add
+/// such c feature in a wrapper and then use the property wrapper on any property where such use case is expected.
+/// For example, let's say we need to define a property but also need to make sure it's never greater than 10.
+/// Below code example shows how to achieve that using a property wrapper.
 
+/// When @propertyWrapper is used then compiler expects type to provide a wrappedValue else it will complain
+/// with error :
+/// Property wrapper type 'LessThanTen' does not contain a non-static property named 'wrappedValue'
+@propertyWrapper
+struct LessThanTen {
+    private var value = 0
+    var wrappedValue: Int {
+        get { return value }
+        set { value = min(newValue, 10) }
+    }
+}
+
+struct UsingCustomPropertyWrapper {
+    @LessThanTen var someNumber: Int
+}
+
+func exampleCustomPropertyWrapper() {
+    var obj = UsingCustomPropertyWrapper()
+    obj.someNumber = 12
+    print("Tried to set value to 12, here's the result : \(obj.someNumber)")
+}
+
+
+/// Examples
 exampleForLazyProperty()
+exampleCustomPropertyWrapper()
 
 //: [Next](@next)
