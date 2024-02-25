a9e1e9dba58f4a3c37c53628a67c918daee5020f
Saurabh Verma <saurabh1088@gmail.com>
2024-02-25 07:55:08 +0530

    Swift Playgrounds : Added unit tests for Stack data structure.

--- DataStructures.playground/Pages/Stack.xcplaygroundpage/Contents.swift
+++ DataStructures.playground/Pages/Stack.xcplaygroundpage/Contents.swift
@@ -22,6 +22,7 @@
  - Function call stack
  */
 import Foundation
+import XCTest
 
 protocol Stackable {
     associatedtype Element
@@ -48,6 +49,8 @@ struct Stack<T>: Stackable {
     
 }
 
+// MARK: -----------------------------------------------------------------------
+// MARK: Example 1 : Stack using Int data type
 func exampleStackDataStructureUsingInt() {
     var stackOfInt = Stack<Int>()
     stackOfInt.push(0)
@@ -57,6 +60,8 @@ func exampleStackDataStructureUsingInt() {
     print(stackOfInt)
 }
 
+// MARK: -----------------------------------------------------------------------
+// MARK: Example 2 : Stack using String data type
 func exampleStackDataStructureUsingString() {
     var stackOfString = Stack<String>()
     stackOfString.push("Batman")
@@ -65,7 +70,51 @@ func exampleStackDataStructureUsingString() {
     print(stackOfString)
 }
 
-exampleStackDataStructureUsingInt()
-exampleStackDataStructureUsingString()
+// MARK: -----------------------------------------------------------------------
+// MARK: Unit Tests
+
+class StackTests: XCTestCase {
+    var sut: Stack<Int>?
+    
+    func test_StackPeek() {
+        sut = Stack<Int>()
+        sut?.push(0)
+        sut?.push(1)
+        sut?.push(2)
+        
+        XCTAssertNotNil(sut?.peek())
+        XCTAssertEqual(2, sut?.peek())
+    }
+    
+    func test_StackPop() {
+        sut = Stack<Int>()
+        sut?.push(0)
+        sut?.push(1)
+        sut?.push(2)
+        
+        let popResult = sut?.pop()
+        XCTAssertNotNil(popResult)
+        XCTAssertEqual(2, popResult)
+    }
+    
+    func test_StackPush() {
+        sut = Stack<Int>()
+        
+        XCTAssertNil(sut?.peek())
+        
+        sut?.push(0)
+        
+        XCTAssertNotNil(sut?.peek())
+        XCTAssertEqual(0, sut?.peek())
+    }
+}
+
+StackTests.defaultTestSuite.run()
+
+// MARK: -----------------------------------------------------------------------
+// MARK: Examples
+
+//exampleStackDataStructureUsingInt()
+//exampleStackDataStructureUsingString()
 
 //: [Next](@next)
