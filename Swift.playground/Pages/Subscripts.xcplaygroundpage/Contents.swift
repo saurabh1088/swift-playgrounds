fe78928a0a2efac932db275f382d14caa9a52169
Saurabh Verma <saurabh1088@gmail.com>
2024-02-25 10:06:27 +0530

    Swift Playgrounds : Added playground to explore swift subscripts.

---
+++ Swift.playground/Pages/Subscripts.xcplaygroundpage/Contents.swift
@@ -0,0 +1,34 @@
+//: [Previous](@previous)
+
+// Created by Saurabh Verma on 25/02/24
+// Copyright © 2023 Saurabh Verma, (saurabh1088@gmail.com). All rights reserved.
+
+/**
+ `Subscripts`
+ 
+ `Subscripts` are shortcuts, used to access elements of a collection. Subscripts can be defined by:
+ 
+ - Classes
+ - Structures
+ - Enumerations
+ 
+ For example elements in an Array can be accessed using index inside square brackets as following
+ 
+ ```
+ var someArray = [1, 2, 3, 4, 5]
+ someArray[0]
+ ```
+ 
+ A type can define more than one subscripts.
+ Subscripts aren't limited to single parameter, multiple input parameters can be also provided.
+ 
+ To define one needs to use keyword `subscript`. The definition itself is similar to the way one defines computed
+ properties.
+ 
+ Subscripts can be read-only OR read-write. In case of read-only one needs to define only the get block.
+ 
+ */
+import Foundation
+
+
+//: [Next](@next)
