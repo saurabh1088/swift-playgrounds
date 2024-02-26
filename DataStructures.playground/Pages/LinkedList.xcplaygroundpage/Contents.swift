ba4b3025abfc789170e5a6d288e805538d4d2231
Saurabh Verma <saurabh1088@gmail.com>
2024-02-26 22:37:59 +0530

    Swift Playgrounds : Added page for linked list

---
+++ DataStructures.playground/Pages/LinkedList.xcplaygroundpage/Contents.swift
@@ -0,0 +1,37 @@
+//: [Previous](@previous)
+
+// Created by Saurabh Verma on 26/02/24
+// Copyright © 2023 Saurabh Verma, (saurabh1088@gmail.com). All rights reserved.
+
+/**
+ Linked list is a data structure used in software engineering which stores a collection of elements. This may sound
+ like an array but there are fundamental differences. An array usually stores elements in a contiguous memory locations
+ whereas linked lists has collections of elements which are connected via references.
+ */
+import Foundation
+
+class Node<Value> {
+    var value: Value
+    var next: Node?
+    
+    init(value: Value, next: Node? = nil) {
+        self.value = value
+        self.next = next
+    }
+}
+
+class LinkedList<Value> {
+    var head: Node?
+    
+    func append(_ value: Value) {
+        let node = Node(value: value)
+        
+        if head?.next == nil {
+            // There isn't any node added to linked list yet
+            head?.next = node
+        } else {
+            
+        }
+    }
+}
+//: [Next](@next)
