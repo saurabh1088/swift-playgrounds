6a787333d711c2bbfb6591230de6e9232203bab6
Saurabh Verma <saurabh1088@gmail.com>
2024-03-18 23:22:33 +0530

    Swift Playgrounds : Added new playground for Swift coding.

---
+++ SwiftCodingPlayground.playground/Contents.swift
@@ -0,0 +1,22 @@
+// Created by Saurabh Verma on 09/05/23
+// Copyright © 2023 Saurabh Verma, (saurabh1088@gmail.com). All rights reserved.
+
+import UIKit
+
+// MARK: -----------------------------------------------------------------------
+// MARK: Example 1 : Reverse print a given string
+
+func reversePrintAGivenStringUsingAppleAPI() {
+    let string = "What a lovely day!!!"
+    let reversedString = string.reversed()
+    print("#####################################################################")
+    print(string)
+    print("---------------------------------------------------------------------")
+    print(String(reversedString))
+    print("#####################################################################")
+}
+
+// MARK: -----------------------------------------------------------------------
+// MARK: Examples
+
+reversePrintAGivenStringUsingAppleAPI()
