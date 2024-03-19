8e53e4c134d0333c434066402aa1fdbd3800cf63
Saurabh Verma <saurabh1088@gmail.com>
2024-03-19 06:58:42 +0530

    Swift Coding Playground : Reverse print a String

--- SwiftCodingPlayground.playground/Contents.swift
+++ SwiftCodingPlayground.playground/Contents.swift
@@ -6,17 +6,24 @@ import UIKit
 // MARK: -----------------------------------------------------------------------
 // MARK: Example 1 : Reverse print a given string
 
+/// `String` instance method `reversed()` is used here for reverse printing the given string. `reversed()`
+/// doesn't directly returns a String, so one needs to cast it to a String before printing.
 func reversePrintAGivenStringUsingAppleAPI() {
     let string = "What a lovely day!!!"
-    let reversedString = string.reversed()
-    print("#####################################################################")
+    let reversedString = String(string.reversed())
     print(string)
-    print("---------------------------------------------------------------------")
-    print(String(reversedString))
-    print("#####################################################################")
+    print(reversedString)
+}
+
+func reversePrintStringWithoutNewAllocation() {
+    let string = "Because, I am Batman!"
+    for char in string.reversed() {
+        print(char, terminator: "")
+    }
 }
 
 // MARK: -----------------------------------------------------------------------
 // MARK: Examples
 
 reversePrintAGivenStringUsingAppleAPI()
+reversePrintStringWithoutNewAllocation()
