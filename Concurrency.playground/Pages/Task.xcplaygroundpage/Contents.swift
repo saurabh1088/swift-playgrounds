0226017f233a45705af22edcfcffaec006aa6cfe
Saurabh Verma <saurabh1088@gmail.com>
2024-04-27 18:52:12 +0530

    Added example for cancelling a task.

--- Concurrency.playground/Pages/Task.xcplaygroundpage/Contents.swift
+++ Concurrency.playground/Pages/Task.xcplaygroundpage/Contents.swift
@@ -13,6 +13,11 @@
  A task runs immediately after it is created and one doesn't need to call any api to get it executed.
  
  The closure given to Task is marked as async as one can see in the definition of Task initialisers.
+ 
+ `Can Task be cancelled?`
+ 
+ Yes, a task can be cancelled, but the responsibility lies with the code running as part of Task itself to check if it's
+ cancelled.
  */
 import Foundation
 
@@ -26,11 +31,11 @@ func exampleSimpleTask() {
 
 // MARK: -----------------------------------------------------------------------
 // MARK: Example 2 : Task returning some value
-let whatIsMyNameTask = Task {
-    return "Batman"
-}
-
 func exampleTaskWithReturnValue() {
+    let whatIsMyNameTask = Task {
+        return "Batman"
+    }
+    
     Task {
         let value = await whatIsMyNameTask.value
         print(value)
@@ -39,32 +44,57 @@ func exampleTaskWithReturnValue() {
 
 // MARK: -----------------------------------------------------------------------
 // MARK: Example 3 : Task performing some operation and then returning value
-let justiceLeague = Task {
-    var justiceLeague = [String]()
-    print("Recruiting Superman.")
-    for index in 1...5 {
-        print("\(index)...")
-        sleep(1)
-    }
-    justiceLeague.append("Superman")
-    
-    print("Recruiting Wonder Woman.")
-    for index in 1...5 {
-        print("\(index)...")
-        sleep(1)
-    }
-    justiceLeague.append("Wonder Woman")
-    
-    return justiceLeague
-}
-
 func exampleTaskWithSomeOperationAndReturnValuePostThat() {
+    let justiceLeague = Task {
+        var justiceLeague = [String]()
+        print("Recruiting Superman.")
+        for index in 1...5 {
+            print("\(index)...")
+            sleep(1)
+        }
+        justiceLeague.append("Superman")
+        
+        print("Recruiting Wonder Woman.")
+        for index in 1...5 {
+            print("\(index)...")
+            sleep(1)
+        }
+        justiceLeague.append("Wonder Woman")
+        
+        return justiceLeague
+    }
+    
     Task {
         let justiceLeagueRecruitedMembers = await justiceLeague.value
         print("Recruited members :: \(justiceLeagueRecruitedMembers)")
     }
 }
 
+// MARK: -----------------------------------------------------------------------
+// MARK: Example 4 :
+func exampleTaskLongRunningWithCheckForCancellation() {
+    let someLongRunningTask = Task {
+        for index in 1...100 {
+            do {
+                try Task.checkCancellation()
+                print("Performing a long runnig task...")
+                print("% Completion : \(index)%")
+                try await Task.sleep(for: .seconds(1))
+            } catch {
+                print("Task is cancelled")
+                break
+            }
+        }
+    }
+    
+    Task {
+        Task {
+            try await Task.sleep(for: .seconds(5))
+            someLongRunningTask.cancel()
+        }
+        await someLongRunningTask.value
+    }
+}
 
 // MARK: -----------------------------------------------------------------------
 // MARK: Examples
@@ -72,5 +102,6 @@ func exampleTaskWithSomeOperationAndReturnValuePostThat() {
 //exampleSimpleTask()
 //exampleTaskWithReturnValue()
 //exampleTaskWithSomeOperationAndReturnValuePostThat()
+//exampleTaskLongRunningWithCheckForCancellation()
 
 //: [Next](@next)
