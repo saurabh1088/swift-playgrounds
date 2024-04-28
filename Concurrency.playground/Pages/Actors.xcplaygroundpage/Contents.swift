f8a31ca2352f18907be2d66b2aadddd855dcbcb5
Saurabh Verma <saurabh1088@gmail.com>
2024-04-28 14:31:56 +0530

    Added examples for where concurrent environment can cause issues and ways to resolve those.

--- Concurrency.playground/Pages/Actors.xcplaygroundpage/Contents.swift
+++ Concurrency.playground/Pages/Actors.xcplaygroundpage/Contents.swift
@@ -9,8 +9,31 @@
 import Foundation
 
 // MARK: -----------------------------------------------------------------------
-// MARK: Example 1 :
-class MessagingService {
+// MARK: Example 1 : Messaging Service without any concurrency considerations
+/*
+ Executing this example randomly gives below errors, sometimes the execution also
+ appears to be stuck.
+ 
+ Error received while execution :
+ 
+ 1.
+ Actors(3323,0x16d0a3000) malloc: *** error for object 0x600001710580: pointer being freed was not allocated
+ Actors(3323,0x16d0a3000) malloc: *** set a breakpoint in malloc_error_break to debug
+ 
+ 2.
+ Actors(4074,0x16f83b000) malloc: Heap corruption detected, free list is damaged at 0x600001710000
+ *** Incorrect guard value: 8261848480
+ Actors(4074,0x16f83b000) malloc: *** set a breakpoint in malloc_error_break to debug
+ Actors(4074,0x16fb83000) malloc: Heap corruption detected, free list is damaged at 0x600001710080
+ *** Incorrect guard value: 72057602293669729
+ Actors(4074,0x16fb83000) malloc: *** set a breakpoint in malloc_error_break to debug
+ */
+protocol MessagingServiceProvider {
+    func received(message: String)
+    func readLast() -> String?
+}
+
+class MessagingService: MessagingServiceProvider {
     private var messages = [String]()
     
     func received(message: String) {
@@ -22,10 +45,10 @@ class MessagingService {
     }
 }
 
-func example1() {
+func exampleWithoutAnyConcurrencyConsiderations() {
     let messagingService = MessagingService()
     let taskOne = Task {
-        for message in ["Apple", "Guava", "Grapes", "Orange", "Strawberry"] {
+        for message in ["1. Apple", "2. Guava", "3. Grapes", "4. Orange", "5. Strawberry"] {
             try await Task.sleep(for: .seconds(1))
             print("Message received : \(message)")
             messagingService.received(message: message)
@@ -33,7 +56,7 @@ func example1() {
     }
     
     let taskTwo = Task {
-        for message in ["Red", "Blue", "Green", "Yellow", "Voilet"] {
+        for message in ["a. Red", "b. Blue", "c. Green", "d. Yellow", "e. Voilet"] {
             try await Task.sleep(for: .seconds(1))
             print("Message received : \(message)")
             messagingService.received(message: message)
@@ -43,17 +66,104 @@ func example1() {
     Task {
         for index in 1...10 {
             try await Task.sleep(for: .seconds(1))
-            print("Last message :: \(messagingService.readLast())")
+            print("\(index). Last message :: \(messagingService.readLast() ?? "EMPTY")")
         }
     }
 }
 
-example1()
+// MARK: -----------------------------------------------------------------------
+// MARK: Example 2 : Fixing Messaging Service
+class MessagingServiceThreadSafe: MessagingServiceProvider {
+    private var messages = [String]()
+    private let queue = DispatchQueue(label: "messaging.service.queue")
+    
+    func received(message: String) {
+        queue.sync {
+            messages.append(message)
+        }
+    }
+    
+    func readLast() -> String? {
+        queue.sync {
+            messages.last
+        }
+    }
+}
+
+func exampleWithConcurrencyConsiderationsUsingSerialQueue() {
+    let messagingService = MessagingServiceThreadSafe()
+    
+    let taskOne = Task {
+        for message in ["1. Apple", "2. Guava", "3. Grapes", "4. Orange", "5. Strawberry"] {
+            try await Task.sleep(for: .seconds(1))
+            print("Message received : \(message)")
+            messagingService.received(message: message)
+        }
+    }
+    
+    let taskTwo = Task {
+        for message in ["a. Red", "b. Blue", "c. Green", "d. Yellow", "e. Voilet"] {
+            try await Task.sleep(for: .seconds(1))
+            print("Message received : \(message)")
+            messagingService.received(message: message)
+        }
+    }
+    
+    Task {
+        for index in 1...10 {
+            try await Task.sleep(for: .seconds(1))
+            print("\(index). Last message :: \(messagingService.readLast() ?? "EMPTY")")
+        }
+    }
+}
+
+// MARK: -----------------------------------------------------------------------
+// MARK: Example 3 : Fixing Messaging Service using Actors
+actor MessagingServiceActor {
+    private var messages = [String]()
+    
+    func received(message: String) {
+        messages.append(message)
+    }
+    
+    func readLast() -> String? {
+        messages.last
+    }
+}
+
+func exampleWithConcurrencyConsiderationsUsingActors() {
+    let messagingService = MessagingServiceActor()
+    
+    let taskOne = Task {
+        for message in ["1. Apple", "2. Guava", "3. Grapes", "4. Orange", "5. Strawberry"] {
+            try await Task.sleep(for: .seconds(1))
+            print("Message received : \(message)")
+            await messagingService.received(message: message)
+        }
+    }
+    
+    let taskTwo = Task {
+        for message in ["a. Red", "b. Blue", "c. Green", "d. Yellow", "e. Voilet"] {
+            try await Task.sleep(for: .seconds(1))
+            print("Message received : \(message)")
+            await messagingService.received(message: message)
+        }
+    }
+    
+    Task {
+        for index in 1...10 {
+            try await Task.sleep(for: .seconds(1))
+            let lastMessage = await messagingService.readLast()
+            print("\(index). Last message :: \(lastMessage ?? "EMPTY")")
+        }
+    }
+}
+
+// MARK: -----------------------------------------------------------------------
+// MARK: Examples
+
+//exampleWithoutAnyConcurrencyConsiderations()
+//exampleWithConcurrencyConsiderationsUsingSerialQueue()
+//exampleWithConcurrencyConsiderationsUsingActors()
 
 //: [Next](@next)
-
-/*
- Error received while execution :
- Actors(3323,0x16d0a3000) malloc: *** error for object 0x600001710580: pointer being freed was not allocated
- Actors(3323,0x16d0a3000) malloc: *** set a breakpoint in malloc_error_break to debug
- */
