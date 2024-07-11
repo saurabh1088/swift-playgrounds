460898b018201037834bb308b4f559c2e7b09ca6
Saurabh Verma <saurabh1088@gmail.com>
2024-07-11 22:48:33 +0530

    Added playground for KVC and KVO.

---
+++ KVCAndKVO.playground/Contents.swift
@@ -0,0 +1,19 @@
+// Created by Saurabh Verma on 11/07/24
+// Copyright © 2023 Saurabh Verma, (saurabh1088@gmail.com). All rights reserved.
+
+/**
+ `Key Value Coding & Key Value Observing`
+ 
+ `Key-Value Coding (KVC)`
+ It is a technique in Objective-C and to some extent in Swift, which allows one to access an object's properties
+ indirectly using string identifiers. This implies one can get or set value of a property using a string rather than
+ accessing the property directly.
+ 
+ `Key-Value Observing (KVO)`
+ It is a mechanism which allows objects to be notified of changes to specified properties of other objects. It's commonly
+ used for implementing the observer pattern, enabling one object to observe changes in another object's properties.
+ 
+ `Is KVC & KVO available in Swift?`
+ Yes KVS and KVO are available in Swift, however it works with some constraints.
+ */
+import UIKit
