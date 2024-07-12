3747f00f7a8a2b6887145753613a249c4fbaa2ce
Saurabh Verma <saurabh1088@gmail.com>
2024-07-12 08:12:31 +0530

    Modified KVC example, added TODO to fix runtime error.

--- KVCAndKVO.playground/Contents.swift
+++ KVCAndKVO.playground/Contents.swift
@@ -21,10 +21,28 @@ import UIKit
 class Employee: NSObject {
     @objc let id: Int
     @objc let name: String
+    @objc let address: Address
     
-    init(id: Int, name: String) {
+    init(id: Int, name: String, address: Address) {
         self.id = id
         self.name = name
+        self.address = address
+    }
+}
+
+class Address: NSObject {
+    @objc let zipcode: Int
+    @objc let addressLine1: String
+    @objc let street: String
+    @objc let city: String
+    @objc let country: String
+    
+    init(zipcode: Int, addressLine1: String, street: String, city: String, country: String) {
+        self.zipcode = zipcode
+        self.addressLine1 = addressLine1
+        self.street = street
+        self.city = city
+        self.country = country
     }
 }
 
@@ -34,13 +52,23 @@ class Employee: NSObject {
 ///
 /// `Terminating app due to uncaught exception 'NSUnknownKeyException', reason: '[<__lldb_expr_7.Employee 0x600000250880> valueForUndefinedKey:]: this class is not key value coding-compliant for the key name.'`
 func exampleKeyValueCodingComplaintClass() {
-    let employee = Employee(id: 1, name: "Batman")
-    if let employeeName = employee.value(forKey: "name"), 
-        let employeeId = employee.value(forKey: "id") {
+    let address = Address(zipcode: 100,
+                          addressLine1: "Wayn Manor",
+                          street: "Dark Street",
+                          city: "Gotham",
+                          country: "USA")
+    let employee = Employee(id: 1, 
+                            name: "Batman", 
+                            address: address)
+    if let employeeName = employee.value(forKey: "name"),
+       let employeeId = employee.value(forKey: "id"),
+        let employeeCity = employee.value(forKey: "address.city") {
         print("Employee details ::")
         print("ID : \(employeeId)")
         print("Name : \(employeeName)")
+        print("City : \(employeeCity)")
     }
 }
 
-exampleKeyValueCodingComplaintClass()
+// TODO: Fix runtime error
+//exampleKeyValueCodingComplaintClass()
