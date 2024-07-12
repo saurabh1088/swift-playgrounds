8f680e51b2e1756e1754296c2d94698fdcf44503
Saurabh Verma <saurabh1088@gmail.com>
2024-07-12 22:03:20 +0530

    Added example for KVO.

--- KVCAndKVO.playground/Contents.swift
+++ KVCAndKVO.playground/Contents.swift
@@ -18,6 +18,8 @@
  */
 import UIKit
 
+// MARK: -----------------------------------------------------------------------
+// MARK: Example 1 : Creating Key Value Coding complaint class and accessing values
 class Employee: NSObject {
     @objc let id: Int
     @objc let name: String
@@ -62,7 +64,7 @@ func exampleKeyValueCodingComplaintClass() {
                             address: address)
     if let employeeName = employee.value(forKey: "name"),
        let employeeId = employee.value(forKey: "id"),
-        let employeeCity = employee.value(forKey: "address.city") {
+       let employeeCity = employee.value(forKeyPath: "address.city") {
         print("Employee details ::")
         print("ID : \(employeeId)")
         print("Name : \(employeeName)")
@@ -70,5 +72,47 @@ func exampleKeyValueCodingComplaintClass() {
     }
 }
 
-// TODO: Fix runtime error
+
+// MARK: -----------------------------------------------------------------------
+// MARK: Example 2 : Key Value Observing
+class ClassWithObservableProperties: NSObject {
+    @objc dynamic var observable: String
+    @objc var nonobservable: String
+    
+    init(observable: String, nonobservable: String) {
+        self.observable = observable
+        self.nonobservable = nonobservable
+    }
+}
+
+/// `Passing reference to non-'@objc dynamic' property 'observable' to KVO method 'observe(_:options:changeHandler:)' may lead to unexpected behavior or runtime trap`
+class ObservingClass: NSObject {
+    @objc let someObservable: ClassWithObservableProperties
+    var observation: NSKeyValueObservation?
+    
+    init(someObservable: ClassWithObservableProperties) {
+        self.someObservable = someObservable
+        super.init()
+        self.observation = self.observe(\.someObservable.observable, options: [.old, .new], changeHandler: { object, change in
+            if let oldValue = change.oldValue, let newValue = change.newValue {
+                print("*******************************************************")
+                print("Old Value : \(oldValue)")
+                print("New Value : \(newValue)")
+            }
+        })
+    }
+}
+
+func exampleKeyValueObserving() {
+    let observableObject = ClassWithObservableProperties(observable: "-", nonobservable: "Start")
+    let observingObject = ObservingClass(someObservable: observableObject)
+    observableObject.observable = "Change 1"
+    observableObject.observable = "Change 2"
+    observableObject.observable = "Change 3"
+}
+
+// MARK: -----------------------------------------------------------------------
+// MARK: Example method calls
+
 //exampleKeyValueCodingComplaintClass()
+exampleKeyValueObserving()
