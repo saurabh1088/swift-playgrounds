609b29b68f5dc1f0062200a2f8a4055665c2a715
Saurabh Verma <saurabh1088@gmail.com>
2025-03-13 11:42:19 +0530

    Added example one.

--- DoubleVSFloat.playground/Contents.swift
+++ DoubleVSFloat.playground/Contents.swift
@@ -1,3 +1,20 @@
 import UIKit
 
-var greeting = "Hello, playground"
+struct FloatValueModel {
+    var value: Float
+}
+
+extension Float {
+    var cleanValue: String {
+        return Int(self.truncatingRemainder(dividingBy: 1)) == 0 ? String(format: "%.2f", self) : String(self)
+    }
+}
+
+func exampleOne() {
+    let valueOne = FloatValueModel(value: 10.99)
+    print(valueOne)
+    print(valueOne.value.cleanValue)
+}
+
+
+exampleOne()
