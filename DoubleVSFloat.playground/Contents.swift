8efffa75b8e78d3b0ddaafd9bca5eda82f127701
Saurabh Verma <saurabh1088@gmail.com>
2025-03-13 12:16:02 +0530

    Added more examples, two, three and four.

--- DoubleVSFloat.playground/Contents.swift
+++ DoubleVSFloat.playground/Contents.swift
@@ -16,5 +16,26 @@ func exampleOne() {
     print(valueOne.value.cleanValue)
 }
 
+func exampleTwo() {
+    let valueOne = FloatValueModel(value: 9999.99)
+    print(valueOne)
+    print(valueOne.value.cleanValue)
+}
+
+func exampleThree() {
+    let valueOne = FloatValueModel(value: 99999.99)
+    print(valueOne)
+    print(valueOne.value.cleanValue)
+}
+
+func exampleFour() {
+    let valueOne = FloatValueModel(value: 999999.99)
+    print(valueOne)
+    print(valueOne.value.cleanValue)
+}
+
 
 exampleOne()
+exampleTwo()
+exampleThree()
+exampleFour()
