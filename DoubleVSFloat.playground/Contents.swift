319696bc9ccaa8a8110cbd8428811a25fec96036
Saurabh Verma <saurabh1088@gmail.com>
2025-03-13 12:32:16 +0530

    Added examples for Double.

--- DoubleVSFloat.playground/Contents.swift
+++ DoubleVSFloat.playground/Contents.swift
@@ -10,32 +10,72 @@ extension Float {
     }
 }
 
-func exampleOne() {
+func exampleFloatOne() {
     let valueOne = FloatValueModel(value: 10.99)
     print(valueOne)
     print(valueOne.value.cleanValue)
 }
 
-func exampleTwo() {
+func exampleFloatTwo() {
     let valueOne = FloatValueModel(value: 9999.99)
     print(valueOne)
     print(valueOne.value.cleanValue)
 }
 
-func exampleThree() {
+func exampleFloatThree() {
     let valueOne = FloatValueModel(value: 99999.99)
     print(valueOne)
     print(valueOne.value.cleanValue)
 }
 
-func exampleFour() {
+func exampleFloatFour() {
     let valueOne = FloatValueModel(value: 999999.99)
     print(valueOne)
     print(valueOne.value.cleanValue)
 }
 
+struct DoubleValueModel {
+    var value: Double
+}
 
-exampleOne()
-exampleTwo()
-exampleThree()
-exampleFour()
+extension Double {
+    var cleanValue: String {
+        return Int(self.truncatingRemainder(dividingBy: 1)) == 0 ? String(format: "%.2f", self) : String(self)
+    }
+}
+
+func exampleDoubleOne() {
+    let valueOne = DoubleValueModel(value: 999999.99)
+    print(valueOne)
+    print(valueOne.value.cleanValue)
+}
+
+func exampleDoubleTwo() {
+    let valueOne = DoubleValueModel(value: 999999999999.99)
+    print(valueOne)
+    print(valueOne.value.cleanValue)
+}
+
+func exampleDoubleThree() {
+    let valueOne = DoubleValueModel(value: 999999999999999999999999.44)
+    print(valueOne)
+    print(valueOne.value.cleanValue)
+}
+
+func exampleDoubleFour() {
+    let valueOne = DoubleValueModel(value: 999999999999999999999999.99)
+    print(valueOne)
+    print(valueOne.value.cleanValue)
+}
+
+
+
+exampleFloatOne()
+exampleFloatTwo()
+exampleFloatThree()
+exampleFloatFour()
+
+exampleDoubleOne()
+exampleDoubleTwo()
+exampleDoubleThree()
+exampleDoubleFour()
