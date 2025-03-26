21e329fe8492bb8a97f5803e85d020f3fc2f9160
Saurabh Verma <saurabh1088@gmail.com>
2025-03-26 22:47:03 +0530

    Added example for checking if two strings are anagrams.

--- SwiftCodingPlayground.playground/Pages/ExamplesPage6.xcplaygroundpage/Contents.swift
+++ SwiftCodingPlayground.playground/Pages/ExamplesPage6.xcplaygroundpage/Contents.swift
@@ -1,7 +1,25 @@
 //: [Previous](@previous)
 
+// Created by Saurabh Verma on 26/04/25
+// Copyright © 2023 Saurabh Verma, (saurabh1088@gmail.com). All rights reserved.
+
 import Foundation
 
-var greeting = "Hello, playground"
+// MARK: -----------------------------------------------------------------------
+// MARK: Example 1 : Check if two strings are anagram
+
+func areAnaagrams(_ string1: String, _ string2: String) -> Bool {
+    let sortedString1 = string1.sorted().reduce("") { partialResult, character in
+        partialResult + String(character)
+    }
+    let sortedString2 = string2.sorted().reduce("") { partialResult, character in
+        partialResult + String(character)
+    }
+    return sortedString1 == sortedString2
+}
+
+// MARK: -----------------------------------------------------------------------
+// MARK: Examples
+areAnaagrams("elbow", "below")
 
 //: [Next](@next)
