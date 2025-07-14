4bfb48d137881ea856bc89e6f3e8ded4bf58eb2f
Saurabh Verma <saurabh1088@gmail.com>
2025-07-14 18:25:58 +0530

    Added example for creating a custom sequence.

--- Swift/Sequence.playground/Pages/SequenceConformance.xcplaygroundpage/Contents.swift
+++ Swift/Sequence.playground/Pages/SequenceConformance.xcplaygroundpage/Contents.swift
@@ -1,9 +1,40 @@
 //: [Previous](@previous)
 
+// Created by Saurabh Verma on 07/14/24
+// Copyright © 2023 Saurabh Verma, (saurabh1088@gmail.com). All rights reserved.
+
 import Foundation
 
-struct CustomSequence: Sequence {
-    
+// MARK: -----------------------------------------------------------------------
+// MARK: Example 1 :
+/// Only conforming to Sequence protocol gives error.
+///
+/// `Type 'CustomSequence' does not conform to protocol 'Sequence'`
+///
+/// ```
+/// struct CustomSequence: Sequence {}
+/// ```
+struct CustomSequence: Sequence, IteratorProtocol {
+    var element: Int = 1
+
+    mutating func next() -> Int? {
+        element = element * 10
+        return element
+    }
 }
+
+func exampleSequence1() {
+    let sequence = CustomSequence()
+    
+    for element in sequence {
+        print(element)
+        if element > 1000 { break }
+    }
+}
+
+// MARK: -----------------------------------------------------------------------
+// MARK: Examples
+exampleSequence1()
+
 
 //: [Next](@next)
